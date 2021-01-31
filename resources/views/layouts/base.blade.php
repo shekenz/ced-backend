6fd7e39ef719ccb3d437381282a414cfd1f8edac
shekenz <aureltrotebas@icloud.com>
2021-01-31 13:30:16 +0100

    Replace reading app name from env by reading it from config.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,12 +1,12 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <title>{{ env('APP_NAME') }}@if(View::hasSection('title')) | @yield('title') @endif</title>
+        <title>{{ config('app.name') }}@if(View::hasSection('title')) | @yield('title') @endif</title>
         <meta charset=UTF-8>
     </head>
     <body></body>
         <div>
-            <h1>{{ env('APP_NAME') }} Index</h1>
+            <h1>{{ config('app.name') }} Index</h1>
             @if (Route::has('login'))
                 <a href="{{ url('/dashboard') }}">Dashboard</a>
             @endif
