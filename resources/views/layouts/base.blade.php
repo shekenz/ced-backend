55c6ccb23b806f8c3fde7506b988a6fc0e80009e
shekenz <aureltrotebas@icloud.com>
2021-01-31 13:21:08 +0100

    Index displays now APP_NAME instead of hardcoded.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,12 +1,12 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <title>@if(View::hasSection('title')) CE.D | @yield('title') @else CE.D @endif</title>
+        <title>{{ env('APP_NAME') }}@if(View::hasSection('title')) | @yield('title') @endif</title>
         <meta charset=UTF-8>
     </head>
     <body></body>
         <div>
-            <h1>CE.D Index</h1>
+            <h1>{{ env('APP_NAME') }} Index</h1>
             @if (Route::has('login'))
                 <a href="{{ url('/dashboard') }}">Dashboard</a>
             @endif
