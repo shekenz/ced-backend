4f4398352fae39e53dc77262c152c3575b66c4a6
shekenz <aureltrotebas@icloud.com>
2021-01-31 15:01:48 +0100

    Added a bit of style to test out tailwind. Looks cool !

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,13 +3,17 @@
     <head>
         <title>{{ config('app.name') }}@if(View::hasSection('title')) | @yield('title') @endif</title>
         <meta charset=UTF-8>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body></body>
         <div>
-            <h1><a href="{{ route('home')}}">{{ config('app.name') }} Index</a></h1>
+            <div class="flex bg-green-300 text-green-900 p-3">
+                <h1 class="flex-none text-2xl"><a href="{{ route('home')}}">{{ config('app.name') }} Index</a></h1>
+                <span class="flex-grow"></span>
             @if (Route::has('login'))
-                <a href="{{ url('/dashboard') }}">Dashboard</a>
+                <a class="flex-none underline" href="{{ url('/dashboard') }}">Dashboard</a>
             @endif
+            </div>
             @yield('content')
         </div>
     </body>
