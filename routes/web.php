17190614735967a0e31306bf6c1ae37acc110974
shekenz <aureltrotebas@icloud.com>
2021-01-31 21:11:42 +0100

    Changed "Name" label to "Username" in register view only (so far, will chnage it later in db). Made the "name" field validation unique, to later log in with username instead of email.

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-})->name('home');
+// Route::get('/', function () {
+//     return view('index');
+// })->name('home');
+Route::view('/', 'index')->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
