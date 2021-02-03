60e8fa585c0eb26700ab80fad694db093dfce9e5
shekenz <aureltrotebas@icloud.com>
2021-02-03 18:32:33 +0100

    Uninstalled editojs. Installed Quill.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,3 @@
 require('./bootstrap');
 
 require('alpinejs');
-
-import EditorJS from '@editorjs/editorjs';
-const editor = new EditorJS();
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,7 +6,8 @@
         <label class="label-shared lg:text-lg" for="title">{{ __('Title') }} :</label>
         <input class="input-shared" id="title" name="title" type="text">
         <label class="label-shared lg:text-lg" for="content">{{ __('Article') }} :</label>
-        <textarea id="editorjs" class="input-shared h-96" name="content"></textarea>
+        <div id="toolbar"></div>
+        <textarea id="editor" class="input-shared h-96" name="content"></textarea>
         <input class="button-shared lg:w-24" type="submit">
     </form>
 </x-app-layout>
