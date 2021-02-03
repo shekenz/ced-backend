42cb4e28d2abd777708007993874bedc77673102
shekenz <aureltrotebas@icloud.com>
2021-02-03 10:34:38 +0100

    Tweaked profile mobile css a lottle bit more.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
 
-            <div class="flex flex-row items-center">
-                <div class="flex-grow md:flex-none">
-                    <img class="rounded-full shadow-md w-32 sm:w-16 sm:mr-4" src="{{ asset('img/default-thumbnail.jpg') }}" alt="Test thumbnail">
+            <div class="flex flex-col sm:flex-row sm:items-center">
+                <div class="m-auto sm:m-0">
+                    <img class="rounded-full shadow-md w-32 sm:w-16 my-2 sm:my-0 sm:mr-4" src="{{ asset('img/default-thumbnail.jpg') }}" alt="Test thumbnail">
                 </div>
-                <div class="flex-grow">
+                <div class="mx-3 sm:mx-0 my-2 sm:my-0">
                     <span class="text-xl">{{ $user->username }}</span><br>
                     <span class="text-gray-500">{{ $user->email }}</span>
                 </div>
