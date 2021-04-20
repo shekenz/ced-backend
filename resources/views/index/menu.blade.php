72cd90e6fd9731ae77df52bcee6b13e07b1f6abd
shekenz <2657910+shekenz@users.noreply.github.com>
2021-04-20 19:36:00 +0200

    Hide also contact menu item from production. "shop" item is renamed "books" in production.

--- resources/views/index/menu.blade.php
+++ resources/views/index/menu.blade.php
@@ -9,9 +9,9 @@
 	xl:mx-20
 ">
 	<h1><a href="{{ route('about') }}" class="{{ (request()->routeIs('about')) ? 'base-menu-link-active' : '' }} base-menu-link base-menu-animated">e.p.g.</a></h1>
-	<div><a href="{{ route('index') }}" class="{{ (request()->routeIs('index')) ? 'base-menu-link-active' : '' }} base-menu-link base-menu-animated">shop</a></div>
-	<div><a href="{{ route('contact') }}" class="base-menu-link {{ (request()->routeIs('contact')) ? 'base-menu-link-active' : '' }} base-menu-animated">contact</a></div>
+	<div><a href="{{ route('index') }}" class="{{ (request()->routeIs('index')) ? 'base-menu-link-active' : '' }} base-menu-link base-menu-animated">{{ (config('app.env') == 'local') ? 'shop' : 'books'}}</a></div>
 	@if(config('app.env') == 'local')
+	<div><a href="{{ route('contact') }}" class="base-menu-link {{ (request()->routeIs('contact')) ? 'base-menu-link-active' : '' }} base-menu-animated">contact</a></div>
 	<div class="md:col-start-8 "><a href="{{ route('cart') }}" class="{{ (request()->routeIs('cart')) ? 'base-menu-link-active ' : '' }}base-menu-link base-menu-animated">cart</a></div>
 	<div class="justify-self-end "><a href="#" class="base-menu-link">fr</a> / <a href="#" class="base-menu-link">en</a></div>
 	@endif
