44bd19b342c40cec8bbbfaa30fbee5766b7e348d
shekenz <2657910+shekenz@users.noreply.github.com>
2021-04-27 13:00:58 +0200

    Invitation mail now generate link with to APP_URL config.

--- resources/views/emails/users/invite.blade.php
+++ resources/views/emails/users/invite.blade.php
@@ -4,7 +4,7 @@
 <br><br>
 {{ __('mails.users.invite.link') }} :
 <br>
-<a href="https://www.epg.works">https://www.epg.works/register/{{ $token }}</a>
+<a href="{{ config('app.url').'/register/'.$token }}">https://www.epg.works/register/{{ $token }}</a>
 <br><br>
 {{ __('mails.users.invite.warning') }}.
 <br><br><br>
