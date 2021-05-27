4fe62bf98fe16687ca2f369e139e647ae33657ce
shekenz <2657910+shekenz@users.noreply.github.com>
2021-05-27 13:04:17 +0200

    Fixed edit menu not fully hidding when publishing is on. Git ignore /config/settings.json

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -41,20 +41,20 @@
 		<div id="menu-wrapper" class="fixed w-full top-0">
 			@auth
 			<div id="user-menu" class="fixed flex w-full base-connected">
-				<div id="user-menu-left">
-					<a id="hide-button" class="base-con-link hideable" title="Hide this menu" href="#"><x-tabler-eye-off class="pb-1 inline w-5 h-5" /></a>
-					<a id="unhide-button" class="base-con-link hideable hidden" title="Un-hide this menu" href="#"><x-tabler-eye class="pb-1 inline w-5 h-5" /></a>
-					<a class="base-con-link hideable" href="{{ url('/dashboard') }}"><x-tabler-gauge class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Dashboard') }}</a>
-					<a class="base-con-link hideable" href="{{ url('/dashboard/books') }}"><x-tabler-book class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Books') }}</a>
-					<a class="base-con-link hideable" href="{{ url('/dashboard/media') }}"><x-tabler-photo class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Media') }}</a>
-					<a class="base-con-link hideable" href="{{ url('/dashboard/settings') }}"><x-tabler-settings class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Settings') }}</a>
+				<a id="hide-button" class="base-con-link hideable" title="Hide this menu" href="#"><x-tabler-eye-off class="pb-1 inline w-5 h-5" /></a>
+				<a id="unhide-button" class="base-con-link hideable hidden" title="Un-hide this menu" href="#"><x-tabler-eye class="pb-1 inline w-5 h-5" /></a>
+				<div id="user-menu-left" class="hideable">
+					<a class="base-con-link" href="{{ url('/dashboard') }}"><x-tabler-gauge class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Dashboard') }}</a>
+					<a class="base-con-link" href="{{ url('/dashboard/books') }}"><x-tabler-book class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Books') }}</a>
+					<a class="base-con-link" href="{{ url('/dashboard/media') }}"><x-tabler-photo class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Media') }}</a>
+					<a class="base-con-link" href="{{ url('/dashboard/settings') }}"><x-tabler-settings class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Settings') }}</a>
 				</div>
-				<div class="flex-grow"></div>
-				<div id="user-menu-right">
-					<a class="flex-none base-con-link hideable" href="{{ route('users.display', Auth::user()->id)}}"><x-tabler-user class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ Auth::user()->username }}</a>
-					<a class="base-con-link hideable" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit()"><x-tabler-logout class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Logout') }}</a><form id="logout-form" class="hidden" action="{{ url('/logout') }}" method="POST">{{ csrf_field() }} </form>
+				<div class="flex-grow hideable"></div>
+				<div id="user-menu-right" class="hideable">
+					<a class="flex-none base-con-link" href="{{ route('users.display', Auth::user()->id)}}"><x-tabler-user class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ Auth::user()->username }}</a>
+					<a class="base-con-link" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit()"><x-tabler-logout class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Logout') }}</a><form id="logout-form" class="hidden" action="{{ url('/logout') }}" method="POST">{{ csrf_field() }} </form>
 					@if( !setting('app.published') )
-					<span class="bg-red-600 text-white rounded-xl ml-2 pb-px pr-3 hideable">
+					<span class="bg-red-600 text-white rounded-xl ml-2 pb-px pr-3">
 						<x-tabler-alert-triangle class="pb-1 ml-2 mr-1 inline w-5 h-5" />{{ __('Website is not published') }}
 					</span>
 					@endif
