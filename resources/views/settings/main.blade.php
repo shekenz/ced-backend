9c086481a806686f080984e0bf5c8381ff06624e
shekenz <2657910+shekenz@users.noreply.github.com>
2021-06-12 12:34:13 +0200

    Redesign of settings form.

--- resources/views/settings/main.blade.php
+++ resources/views/settings/main.blade.php
@@ -6,11 +6,17 @@
 	<form method="POST" action="{{ route('settings.update') }}">
 			@csrf
 			@method('patch')
-			<label class="label-shared lg:text-lg" for="about-0">{{ __('About: First Column') }}</label>
-			<textarea class="input-shared h-96" id="about-0" name="about[]">{!! (Storage::disk('raw')->exists('about_0.txt')) ? Storage::disk('raw')->get('about_0.txt') : '' !!}</textarea>
-			<label class="label-shared lg:text-lg" for="about-1">{{ __('About: Second Column') }}</label>
-			<textarea class="input-shared h-96" id="about-1" name="about[]">{!! (Storage::disk('raw')->exists('about_1.txt')) ? Storage::disk('raw')->get('about_1.txt') : '' !!}</textarea>
-			<div class="text-right">
+			<div class="flex gap-x-4">
+				<div class="flex-grow">
+					<label class="label-shared lg:text-lg" for="about-0">{{ __('About: First Column') }}</label>
+					<textarea class="input-shared h-96" id="about-0" name="about[]">{!! (Storage::disk('raw')->exists('about_0.txt')) ? Storage::disk('raw')->get('about_0.txt') : '' !!}</textarea>
+				</div>
+				<div class="flex-grow">
+					<label class="label-shared lg:text-lg" for="about-1">{{ __('About: Second Column') }}</label>
+					<textarea class="input-shared h-96" id="about-1" name="about[]">{!! (Storage::disk('raw')->exists('about_1.txt')) ? Storage::disk('raw')->get('about_1.txt') : '' !!}</textarea>
+				</div>
+			</div>
+			<div class="text-right mt-4">
 				<input class="button-shared" type="submit">
 			</div>
 	</form>
