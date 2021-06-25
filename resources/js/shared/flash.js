89945d786b26469f3edae2a44e2b50d3a5fdbabe
shekenz <2657910+shekenz@users.noreply.github.com>
2021-06-25 19:57:13 +0200

    Added a permanent tag on flash attributes for sticky flash messages

--- resources/js/shared/flash.js
+++ resources/js/shared/flash.js
@@ -3,7 +3,8 @@ let flash = document.getElementById('flash-wrapper');
 if(flash) {
 	let flashes = flash.childNodes;
 	flashes.forEach((item, index) => {
-		if(item.tagName) {
+		console.log(item.parentElement);
+		if(item.tagName && !item.parentElement.classList.contains('permanent')) {
 			setTimeout(function() {
 				item.classList.add('opacity-0');
 				setTimeout(function() {
--- resources/views/components/flash/back.blade.php
+++ resources/views/components/flash/back.blade.php
@@ -1,4 +1,4 @@
-<div id="flash-wrapper">
+<div id="flash-wrapper" class="@if($attributes->has('permanent')) {{ 'permanent' }} @endif">
 	<div {{ $attributes->merge(['class' => 'text-gray-900 p-2 rounded-lg mb-4 text-center transition-all duration-300']) }}>
 		{{ $message }}
 	</div>
