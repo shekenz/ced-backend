d9f4f73b09e7e79ef7b4ad307f9e8dc97b012a87
shekenz <2657910+shekenz@users.noreply.github.com>
2021-07-02 11:40:24 +0200

    Index flash permanent ability

--- resources/views/components/flash/default.blade.php
+++ resources/views/components/flash/default.blade.php
@@ -1,4 +1,4 @@
-<div id="flash-wrapper" class="fixed w-full px-6 top-6 z-50">
+<div id="flash-wrapper" class="fixed w-full px-6 top-6 z-50 @if($attributes->has('permanent')) {{ 'permanent' }} @endif">
 	<div {{ $attributes->merge(['class' => 'text-gray-900 p-6 border-2 m-auto text-center transition-all duration-300 mb-4']) }}>
 		{{ $message }}
 	</div>
