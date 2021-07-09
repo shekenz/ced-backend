b5f71153347a94f5099688b6b60e8e09b245af35
shekenz <2657910+shekenz@users.noreply.github.com>
2021-07-09 18:31:30 +0200

    Update moved new order script out of module for better performance

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,5 +2,22 @@
 require('./index/menu');
 require('./index/theme');
 require('./shared/flash');
-require('./shared/new-orders');
+//require('./shared/new-orders');
 
+let orderUnread = document.getElementById('orderUnread');
+
+if(orderUnread) {
+	window.fetch('/api/orders/unread/count', {
+		method: 'post',
+		headers: {
+			'accept': 'application/json',
+		},
+	}).then(response => {
+		return response.json();
+	}).then(jsonResponse => {
+		if(jsonResponse.count && jsonResponse.count > 0) {
+			orderUnread.classList.remove('hidden');
+			orderUnread.appendChild(document.createTextNode(jsonResponse.count));
+		}
+	});
+}
